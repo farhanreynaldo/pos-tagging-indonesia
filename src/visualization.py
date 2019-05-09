28597ef05888d89243905d09d9e98fafdc0369a1
farhanreynaldo <xynxnx@gmail.com>
2019-05-09 11:10:18 +0700

    Normalization for confusion matrix function

--- src/visualization.py
+++ src/visualization.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from functools import wraps
 from utils import flatten
 import seaborn as sns
+import numpy as np
 
 def _flattens_y(func):
     @wraps(func)
@@ -16,9 +17,10 @@ def _flattens_y(func):
 def plot_confusion_matrix(y_true, y_pred, labels):
 
     matrix = confusion_matrix(y_true, y_pred, labels=labels)
+    matrix = matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis]
     fig, ax = plt.subplots(figsize=(16, 12))
 
-    sns.heatmap(matrix, annot=True, xticklabels=labels, yticklabels=labels, fmt="d", ax=ax)
+    sns.heatmap(matrix, xticklabels=labels, yticklabels=labels, fmt="d", cmap="YlGnBu", ax=ax)
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     return ax
